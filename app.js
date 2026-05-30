775babeb4220657e5c5fd48bba2178515eb50b58
Shubhamyadav47 <158451403+Shubhamyadav47@users.noreply.github.com>
2026-05-30 10:28:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC-p-xjcMGmb9WevrcchPNCrm3rk_tqMDg",
   authDomain: "photoselectiontool-f6b36.firebaseapp.com",
   projectId: "photoselectiontool-f6b36",
   storageBucket: "photoselectiontool-f6b36.firebasestorage.app",
   messagingSenderId: "1031705182047",
-  appId: "1:1031705182047:web:e9f099ce146cc77bfb80ea",
-  measurementId: "G-X7TZT82QC3"
+  appId: "1:1031705182047:web:e9f099ce146cc77bfb80ea"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+console.log("Firebase Connected Successfully");
