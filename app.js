00805d3d53b9255369cfd70e555d2142e4554851
Shubhamyadav47 <158451403+Shubhamyadav47@users.noreply.github.com>
2026-05-30 10:46:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-p-xjcMGmb9WevrcchPNCrm3rk_tqMDg",
@@ -10,5 +17,32 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-console.log("Firebase Connected Successfully");
+window.loginWithGoogle = async function () {
+  try {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+window.logoutUser = async function () {
+  await signOut(auth);
+};
+
+onAuthStateChanged(auth, (user) => {
+  const loginWall = document.getElementById("loginWall");
+  const toolContainer = document.getElementById("toolContainer");
+  const userEmail = document.getElementById("userEmail");
+
+  if (user) {
+    loginWall.style.display = "none";
+    toolContainer.style.display = "block";
+    userEmail.textContent = user.email;
+  } else {
+    loginWall.style.display = "flex";
+    toolContainer.style.display = "none";
+  }
+});
